348a3b52873c09a24e2176fe612fb409f4e08850
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-03-30 14:01:55 -0300

    avance inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './App.css';
+import { GlobalContext } from './components/context/GlobalContext';
 import VistaEncuesta from './components/ui/vistaEncuesta/VistaEncuesta';
 
 function App() {
+
+  const [appStage, setAppStage] = useState(0); // este valor luego debe venir por localstorage para tener control si se abre desde clientes o desde encuestas
+
+
   return (
     <>
-      <VistaEncuesta/>
+    <GlobalContext.Provider
+      value = {{
+        appStage, 
+        setAppStage,
+      }}
+    >      
+      <VistaEncuesta />
+    </GlobalContext.Provider>
     </>
   );
 }
--- src/components/ui/vistaEncuesta/VistaEncuesta.js
+++ src/components/ui/vistaEncuesta/VistaEncuesta.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../../context/GlobalContext';
+import './style.css';
 
 const VistaEncuesta = () => {
+    const { appStage, setAppStage } = useContext(GlobalContext);
+
     return (
-        <div>
-            Vista Encuesta
-        </div>
+        <>
+            {appStage === 0 ?(
+                <div className='div_wrapper'>
+                    <h1>Vista completa de encuesta</h1>
+                </div>
+
+            ):(
+                <div className='div_wrapper'>
+                    <h1>Vista en partes de la encuesta</h1>
+                </div>
+            )}
+        </>
     );
 };
 
