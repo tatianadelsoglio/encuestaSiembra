e16dce416b9ecaf50d6fb01c9a3ed0cbd7b4e79b
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-04-04 10:23:52 -0300

    modificando form editar

--- src/components/ui/tablaEncuesta/TablaEncuesta.js
+++ src/components/ui/tablaEncuesta/TablaEncuesta.js
@@ -1,7 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import "./style.css";
-import {Drawer, Space, Table, Tag } from "antd";
+import {
+  Button,
+  DatePicker,
+  Drawer,
+  Form,
+  Input,
+  Select,
+  Space,
+  Table,
+  Tag,
+} from "antd";
 import {
   DeleteOutlined,
   EditOutlined,
@@ -12,6 +22,150 @@ import {
 } from "@ant-design/icons";
 
 const TablaEncuesta = () => {
+  //a esta data le falta cosecha y array de lotes
+  const data = [
+    {
+      key: "1",
+      name: "John Brown",
+      cultivo: "MAIZ",
+      ciclo: "1",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      cultivo: "SOJA",
+      ciclo: "2",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      cultivo: "TRIGO",
+      ciclo: "",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "4",
+      name: "Joe Black",
+      cultivo: "SOJA",
+      ciclo: "1",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "5",
+      name: "John Brown",
+      cultivo: "TRIGO",
+      ciclo: "",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "6",
+      name: "John Brown",
+      cultivo: "MAIZ",
+      ciclo: "2",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "7",
+      name: "Joe Black",
+      cultivo: "SOJA",
+      ciclo: "1",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "8",
+      name: "Joe Black",
+      cultivo: "TRIGO",
+      ciclo: "",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "9",
+      name: "Joe Black",
+      cultivo: "SOJA",
+      ciclo: "1",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "10",
+      name: "John Brown",
+      cultivo: "TRIGO",
+      ciclo: "",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "11",
+      name: "Jim Green",
+      cultivo: "SOJA",
+      ciclo: "1",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "12",
+      name: "Joe Black",
+      cultivo: "SOJA",
+      ciclo: "2",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "13",
+      name: "Jim Green",
+      cultivo: "MAIZ",
+      ciclo: "1",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+    {
+      key: "14",
+      name: "Jim Green",
+      cultivo: "MAIZ",
+      ciclo: "2",
+      sup: 210,
+      rinde: 200,
+      costo: 300,
+      estado: ["ok"],
+    },
+  ];
+
   const columns = [
     {
       title: "Cliente",
@@ -24,6 +178,12 @@ const TablaEncuesta = () => {
       dataIndex: "cultivo",
       key: "cultivo",
       align: "center",
+      render: (text, record) => {
+        if (record.cultivo === 'TRIGO') {
+          return text;
+        }
+        return `${text} / ${record.ciclo}°`;
+      },
     },
     {
       title: "SUP. EST.(HAS)",
@@ -98,134 +258,7 @@ const TablaEncuesta = () => {
       align: "center",
     },
   ];
-  const data = [
-    {
-      key: "1",
-      name: "John Brown",
-      cultivo: "MAIZ / 1º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      cultivo: "SOJA / 2º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "3",
-      name: "Joe Black",
-      cultivo: "Trigo",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "4",
-      name: "Joe Black",
-      cultivo: "SOJA / 1º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "5",
-      name: "John Brown",
-      cultivo: "Trigo",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "6",
-      name: "John Brown",
-      cultivo: "	MAIZ / 2º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "7",
-      name: "Joe Black",
-      cultivo: "SOJA / 1º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "8",
-      name: "Joe Black",
-      cultivo: "TRIGO",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "9",
-      name: "Joe Black",
-      cultivo: "SOJA / 1º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "10",
-      name: "John Brown",
-      cultivo: "TRIGO",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "11",
-      name: "Jim Green",
-      cultivo: "SOJA / 1º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "12",
-      name: "Joe Black",
-      cultivo: "SOJA / 2º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "13",
-      name: "Jim Green",
-      cultivo: "MAIZ / 1º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-    {
-      key: "14",
-      name: "Jim Green",
-      cultivo: "MAIZ / 2º",
-      sup: 210,
-      rinde: 200,
-      costo: 300,
-      estado: ["ok"],
-    },
-  ];
+  
 
   const [isDrawerVisible, setIsDrawerVisible] = useState(false);
   const [title, setTitle] = useState("");
@@ -235,6 +268,9 @@ const TablaEncuesta = () => {
   const [recordToAddFile, setRecordToAddFile] = useState(null);
   const [recordToVerLote, setRecordToVerLote] = useState(null);
   //const [recordToDeleteEnc, setRecordToDeleteEnc] = useState(null);
+
+  const { Option } = Select;
+  const [form] = Form.useForm();
 
   const handleEdit = (record) => {
     setTitle("Editar Encuesta Siembra");
@@ -278,8 +314,12 @@ const TablaEncuesta = () => {
     setRecordToVerEnc(null);
     handleAddFile(null);
     setRecordToVerLote(null);
-   // handleDeleteEnc(null);
+    // handleDeleteEnc(null);
     setIsDrawerVisible(false);
+  };
+
+  const onFinishEdit = (values) => {
+    console.log("Received values of form: ", values);
   };
 
   return (
@@ -299,10 +339,113 @@ const TablaEncuesta = () => {
         >
           {/* Aquí va el formulario para editar el registro */}
           {recordToEdit && (
-            <div>
-              <p>Nombre: {recordToEdit.name}</p>
-              <p>Cultivo: {recordToEdit.cultivo}</p>
-              {/* ... y así sucesivamente para los demás campos */}
+            
+            <div className="div_drawerWrapper">
+              {console.log(recordToEdit)}
+              <Form
+                form={form}
+                name="validate_other"
+                layout="vertical"
+                //   {...formItemLayout}
+                onFinish={onFinishEdit}
+              >
+                <Form.Item name="estado" label="Estado">
+                  <Select
+                    placeholder="Seleccione Estado"
+                    style={{ width: "300px" }}
+                    defaultValue={recordToEdit.estado}
+                  >
+                    <Option value="1">ENCUESTA OK</Option>
+                    <Option value="2">NO ENCUESTADO</Option>
+                    <Option value="3">NO ACCEDE</Option>
+                    <Option value="4">NO SIEMBRA</Option>
+                  </Select>
+                </Form.Item>
+
+                <Form.Item name="cliente" label="Cliente">
+                  <Select
+                    disabled
+                    placeholder="Seleccione Cliente"
+                    style={{ width: "300px" }}
+                    defaultValue={recordToEdit.name}
+                  >
+                  </Select>
+                </Form.Item>
+
+                <Form.Item name="lote" label="Lotes">
+                  <Select
+                    disabled
+                    mode="multiple"
+                    placeholder="Seleccione Lote/s"
+                    defaultValue={["LOTE 1", "LOTE 2"]}
+                    style={{ width: "300px" }}
+                  >
+                  </Select>
+                </Form.Item>
+                <div className="divFormEstruc">
+                  <Form.Item name="cosecha" label="Cosecha">
+                    <Select
+                      disabled
+                      placeholder="Seleccione Cosecha"
+                      defaultValue={"2223"}
+                      style={{ width: "150px" }}
+                    >
+                      <Option value="1">2223</Option>
+                      <Option value="2">2122</Option>
+                      <Option value="3">2021</Option>
+                      <Option value="4">1920</Option>
+                    </Select>
+                  </Form.Item>
+                  <Form.Item name="cultivo" label="Cultivo">
+                    <Select
+                      disabled
+                      placeholder="Seleccione Cultivo"
+                      defaultValue={recordToEdit.cultivo}
+                      style={{ width: "150px" }}
+                    >
+                    </Select>
+                  </Form.Item>
+                </div>
+                <div className="divFormEstruc">
+                  <Form.Item name="ciclo" label="Ciclo">
+                    <Select
+                      disabled
+                      placeholder="Seleccione Ciclo"
+                      defaultValue={recordToEdit.ciclo}
+                      style={{ width: "150px" }}
+                    >
+                    </Select>
+                  </Form.Item>
+                  <Form.Item
+                    name="hasEstimadas"
+                    label="Has. Estimadas"
+                    style={{ marginLeft: "31px" }}
+                  >
+                    <Input defaultValue={recordToEdit.sup} style={{ width: "150px" }} />
+                  </Form.Item>
+                </div>
+                <div className="divFormEstruc">
+                  <Form.Item name="rindeTT" label="Rinde (TT)">
+                    <Input defaultValue={recordToEdit.rinde} style={{ width: "150px" }} />
+                  </Form.Item>
+                  <Form.Item
+                    name="costo"
+                    label="Costo (U$S)"
+                    style={{ marginLeft: "31px" }}
+                  >
+                    <Input defaultValue={recordToEdit.costo} style={{ width: "150px" }} />
+                  </Form.Item>
+                </div>
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{ width: "100%", marginTop: "20px" }}
+                  >
+                    Guardar
+                  </Button>
+                </Form.Item>
+              </Form>
             </div>
           )}
           {recordToAddEvent && (
@@ -333,7 +476,6 @@ const TablaEncuesta = () => {
               {/* ... y así sucesivamente para los demás campos */}
             </div>
           )}
-          
         </Drawer>
       </div>
     </>
